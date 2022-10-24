840667aaba498b035906b62f4bf9af222de4fa56
hiruna jayasuriya <62193955+Sandu727@users.noreply.github.com>
2022-10-24 11:33:28 +0530

    add & reduce quantity - inventory

--- query/pharmacyData.js
+++ query/pharmacyData.js
@@ -5,8 +5,9 @@ exports.GET_ALL_ORDERS = "SELECT * FROM  orders JOIN customer ON orders.customer
 exports.GET_ORDER = "SELECT * FROM  orders JOIN customer ON orders.customer_id = customer.uid WHERE pharmacy_id =? AND orders.order_id=?";
 
 exports.ADD_NEW_INVENTORY_ITEM = "INSERT INTO inventory(batch_No, pharmacy_id, brand_name, drug_name, quantity, expiry_date, manufacture_date, licenece_No, unit_price) VALUES (?,?,?,?,?,?,?,?,?)"
-exports.GET_INVENTORY_ITEM = "SELECT * FROM inventory WHERE pharmacy_id = ? AND expiry_date = ? AND batch_No = ?";
+exports.GET_INVENTORY_ITEM = "SELECT * FROM inventory WHERE pharmacy_id = ? AND batch_No = ?";
 exports.GET_ALL_INVENTORY_ITEMS = "SELECT * FROM inventory WHERE pharmacy_id = ?";
 exports.GET_LOWEST_INVENTORY_ITEMS = "SELECT * FROM inventory WHERE pharmacy_id = ? ORDER BY quantity ASC LIMIT 5";
 exports.DELETE_INVENTORY_ITEM = "DELETE FROM inventory WHERE pharmacy_id =? AND batch_No = ?";
 exports.UPDATE_INVENTORY_ITEM = "UPDATE `inventory` SET brand_name=?,drug_name=?,quantity=?,expiry_date=?,manufacture_date=?,licenece_No=?,unit_price=? WHERE pharmacy_id =? AND batch_No = ?";
+exports.UPDATE_QUANTITY = "UPDATE `inventory` SET quantity=? WHERE pharmacy_id = ? AND batch_No = ?";
